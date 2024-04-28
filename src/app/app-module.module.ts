9e1e04cc7dc7fae310964aff25c41901d2525c20
pradhan prasanta <pradhan.prasanta78@gmail.com>
2024-04-28 22:25:27 +0530

    day 1

---
+++ src/app/app-module.module.ts
@@ -0,0 +1,21 @@
+//import { NgModule } from '@angular/core';
+//import { CommonModule } from '@angular/common';
+//import { IfExampleComponent } from './if-example/if-example.component';
+//import { SignalsExampleComponent } from './signals-example/signals-example.component';
+//import { SwitchExampleComponent } from './switch-example/switch-example.component';
+//import { ForExampleComponent } from './for-example/for-example.component';
+
+
+
+//@NgModule({
+//  declarations: [
+//    IfExampleComponent,
+//    ForExampleComponent,
+//    SwitchExampleComponent,
+//    SignalsExampleComponent
+//  ],
+//  imports: [
+//    CommonModule
+//  ]
+//})
+//export class AppModuleModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { ForExampleComponent } from './for-example/for-example.component';
+import { IfExampleComponent } from './if-example/if-example.component';
+import { SwitchExampleComponent } from './switch-example/switch-example.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ForExampleComponent, IfExampleComponent, SwitchExampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
   title = 'angular-17';
 }
